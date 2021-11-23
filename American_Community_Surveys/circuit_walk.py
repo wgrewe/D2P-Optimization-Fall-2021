825f576062a0b0e886031c2e9f504f2cf97889cc
DillWithIt77 <74215013+DillWithIt77@users.noreply.github.com>
2021-11-22 20:45:24 -0700

    Create circuit_walk.py

---
+++ American_Community_Surveys/circuit_walk.py
@@ -0,0 +1,23 @@
+def circuit_walk(vert1,vert2, circuits,B):
+	s = vert1
+	u = vert2-s
+	circ_set = []
+	lamb = []
+	g = 0
+
+	while (s != vert2):
+		for i in range(len(circuits)):
+			if ((B*u)*(B*circuitc[i])>=0):
+				g = circuits[i]
+				break;
+		for i in range(len(g)):
+			lamb[i] = (s[i]-vert2[i])/g[i]
+
+		min_lambda = min(lamb)
+
+		s = s+lamb*g
+		u = vert2-s
+		circ_set.append(lamb*g)
+
+	return circ_set
+
