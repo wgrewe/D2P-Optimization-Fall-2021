9a3e304bd5027a6a4085a4b6bb9cc298b9f1a443
DillWithIt77 <74215013+DillWithIt77@users.noreply.github.com>
2021-11-22 20:03:29 -0700

    Update cycles_graph.py

--- American_Community_Surveys/cycles_graph.py
+++ American_Community_Surveys/cycles_graph.py
@@ -1,24 +1,12 @@
-def isCircular(arr1, arr2):
-    if len(arr1) != len(arr2):
-        return False
-
-    str1 = ' '.join(map(str, arr1))
-    str2 = ' '.join(map(str, arr2))
-    if len(str1) != len(str2):
-        return False
-
-    return str1 in str2 + ' ' + str2
-
 def cycles_graph(graph, nodes):
 	'''
 	input: a graph, and the number of nodes to walk through
 	output: all simple cycles paths in the graph 
 	logic: use the depth first to find all paths starting at each vertex.
-	Use filter to remove all duplicated walks. Since walks are cycle free the
-	only way to have a repeat is if the reversed walk was already performed, 
-	therefore, repeated walks are those where the end is less than the start.
-	Also, filter out all circuits that do not end on a cluster, these are circuits of 
-	even length.
+	Use filter to remove walks of length 3 or less since a  simple cycle will need at least 4 vertices at it's smallest.
+	Remove all nodes that don't end at a cluster. We end on a cluster when the length of the list of vertices is an even number
+	since this corresponds to an odd number of edges. 
+	Lastly, add in the start node to the end of each list to represent the cluster node returning to the start node of the cycle. 
 	'''
 	paths = []
 	multi_edge_paths = []
@@ -35,15 +23,5 @@ def cycles_graph(graph, nodes):
 	for i in range(len(odd_length_paths)):
 		odd_length_paths[i].append(odd_length_paths[i][0])
 	return odd_length_paths
-    
-# 	removed_paths = [] 
-# 	for i in range(len(odd_length_paths)):
-# 		for j in range(len(odd_length_paths)):
-# 			if(isCircular(odd_length_paths[i],odd_length_paths[j])):
-# 				removed_paths.append(odd_length_paths[i])
-                
-# 	for i in range(len(removed_paths)):
-# 		if removed_paths[i] in odd_length_paths:
-# 			odd_length_paths.remove(removed_paths[i])
 
 	
