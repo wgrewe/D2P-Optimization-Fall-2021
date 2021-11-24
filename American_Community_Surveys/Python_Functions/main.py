85bea2d8d9c97d8f992303f33bec52f4f9469d36
DillWithIt77 <74215013+DillWithIt77@users.noreply.github.com>
2021-11-24 11:06:33 -0700

    fixed calls

--- American_Community_Surveys/Python_Functions/main.py
+++ American_Community_Surveys/Python_Functions/main.py
@@ -1,9 +1,9 @@
 import numpy as np
-import constraint_mat
-import sign_comp_circs
-import circuit_walk
-import cycles_graph
-import get_sequential_circuits
+import constraint_mat as cm
+import sign_comp_circs as scc
+import circuit_walk as cw
+import cycles_graph as cg
+import get_sequential_circuits as gsc
 
 nodes = 5
 clusters = 3
@@ -12,13 +12,13 @@ vert1 = [0,0,1,0,1,0,1,0,0,0,0,1,0,1,0]
 vert2 = [0,0,1,1,0,0,1,0,0,1,0,0,0,1,0]
 
 def main(nodes,clusters,vert1,vert2):
-	graph = build_graph(nodes,clusters)
-	cyc_graph = cycles_graph(graph,nodes)
-	circuits = get_sequential_circuits(nodes,clusters)
+	graph = gsc.build_graph(nodes,clusters)
+	cyc_graph = cg.cycles_graph(graph,nodes)
+	circuits = gsc.get_sequential_circuits(nodes,clusters)
 	#make one for cycles#
-	B = constraint_mat(nodes,clusters)
-	sign_comp = sign_comp_circs(vert2-vert1,circuits,B)
-	circ_walk = circuit_walk(vert1,vert2,sign_comp)
+	B = cm.constraint_mat(nodes,clusters)
+	sign_comp = scc.sign_comp_circs(vert2-vert1,circuits,B)
+	circ_walk = cw.circuit_walk(vert1,vert2,sign_comp)
 	return circ_walk
 
 
