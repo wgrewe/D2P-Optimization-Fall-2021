8b49d709eec6eef7447b75828ab1a1a499a1e9bc
DillWithIt77 <74215013+DillWithIt77@users.noreply.github.com>
2021-11-24 10:34:42 -0700

    added piece for greater than zero

--- American_Community_Surveys/Python_Functions/constraint_mat.py
+++ American_Community_Surveys/Python_Functions/constraint_mat.py
@@ -3,14 +3,13 @@ def constraint_mat(nodes, clusters):
 	Input: Number of nodes and clusters for the partition polytope
 	Output: Constraint matrix for the partition polytope
 	'''
-	B = np.zeros((clusters,nodes*clusters))
+	B = np.zeros((nodes*clusters+clusters,nodes*clusters))
 
 	for i in range(clusters):
 		for j in range(nodes):
 			B[i,j*clusters+i] = 1
 
-# 	for i in range(nodes):
-# 		for j in range(clusters):
-# 			B[i+clusters,j+(i*clusters)] = 1
+	for i in range(nodes*clusters):
+		B[i+clusters,i] = -1
 
 	return B
