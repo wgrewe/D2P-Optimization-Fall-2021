93c62a05ea41f16d9e362f299376a21cb4d40440
DillWithIt77 <74215013+DillWithIt77@users.noreply.github.com>
2021-11-23 17:26:37 -0700

    fixed checks based on zoom meeting

--- American_Community_Surveys/circuit_walk.py
+++ American_Community_Surveys/circuit_walk.py
@@ -2,23 +2,26 @@ def circuit_walk(vert1,vert2, circuits,B):
 	s = vert1
 	u = vert2-s
 	circ_set = []
+	picked_circs = []
 	lamb = []
 	g = 0
 
 	while (s != vert2):
 		for i in range(len(circuits)):
-			if (B.dot(u).dot(transpose(B.dot(circuits[i])))>=0):
+			if (circuit[i] not in picked_circs):
 				g = circuits[i]
+				picked_circs.append(g)
 				break;
 		for i in range(len(g)):
-			lamb.append((s[i]-vert2[i])/g[i])
+			if (u[i] != 0):
+				lamb.append((s[i]-vert2[i])/g[i])
 		
 		pos_lamb = list(filter(lambda c: c > 0, lamb))
 		min_lambda = min(pos_lamb)
 
-		s = s+lamb*g
+		s = s+min_lamb*g
 		u = vert2-s
-		circ_set.append(lamb*g)
+		circ_set.append(min_lamb*g)
 
 	return circ_set
 
