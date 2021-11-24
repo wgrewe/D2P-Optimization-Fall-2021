e10476733a15ff0808031b87d529dd6cf34faf98
DillWithIt77 <74215013+DillWithIt77@users.noreply.github.com>
2021-11-23 22:26:56 -0700

    helper function for sign-compatibility and making a constraint matrix

---
+++ American_Community_Surveys/sign_comp_circs.py
@@ -0,0 +1,10 @@
+def sign_comp_circs(vertex, circuits, B):
+	'''
+	Input: vertex, set of circuits, constraint matrix B
+	Output: set of sign-compatiable circuits to vertex with respect to B
+	'''
+	sign_comp_circs = []
+		for i in circuits:
+			if (B.dot(vertex)).dot(transpose(B.dot(i)))>=0:
+				sign_comp_circs.append(i)
+	return sign_comp_circs
