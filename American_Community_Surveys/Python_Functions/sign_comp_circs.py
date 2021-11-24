c8f1a674a7533154b81c5318cf5d0f2f3cdd45f3
DillWithIt77 <74215013+DillWithIt77@users.noreply.github.com>
2021-11-24 10:15:53 -0700

    fixed indents for Weston

--- American_Community_Surveys/Python_Functions/sign_comp_circs.py
+++ American_Community_Surveys/Python_Functions/sign_comp_circs.py
@@ -4,7 +4,7 @@ def sign_comp_circs(vertex, circuits, B):
 	Output: set of sign-compatiable circuits to vertex with respect to B
 	'''
 	sign_comp_circs = []
-		for i in circuits:
-			if (B.dot(vertex)).dot(transpose(B.dot(i)))>=0:
-				sign_comp_circs.append(i)
+	for i in circuits:
+		if (B.dot(vertex)).dot(transpose(B.dot(i)))>=0:
+			sign_comp_circs.append(i)
 	return sign_comp_circs
