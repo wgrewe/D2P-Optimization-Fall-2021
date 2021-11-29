869b34b1e4b7d1bcaa5079ed4966895688e467d9
DillWithIt77 <74215013+DillWithIt77@users.noreply.github.com>
2021-11-28 20:23:44 -0700

    Update clust_to_vect.py

--- American_Community_Surveys/Python_Functions/clust_to_vect.py
+++ American_Community_Surveys/Python_Functions/clust_to_vect.py
@@ -1,10 +1,20 @@
+import numpy as np
 def cluster_to_vector(cluster_assign, num_clusters):
-	num_nodes = len(cluster)
-	cluster = np.array(cluster)
+	num_nodes = len(cluster_assign)
+	cluster = np.array(cluster_assign)
 
 
 	binary_assignments = np.zeros((num_nodes, num_clusters))
 	for i in range(num_nodes):
-		binary_assignments[i, cluster[i]] = 1
+		binary_assignments[i, cluster[i]-1] = 1
 
-	return binary_assignments
+	correct_vec =np.reshape(binary_assignments,(1,num_nodes*num_clusters)) 	
+
+	return correct_vec
+
+num_clusters = 3
+
+test = [1,2,1,2,3,1,3,3]
+
+cluster_vec = cluster_to_vector(test,num_clusters)
+print(cluster_vec)
